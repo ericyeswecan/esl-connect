26bc7849d7d4b7fd3d66fa73d2dce5f3fcb93aa4
ericyeswecan <ericyeswecan@gmail.com>
2026-02-11 11:34:46 +0900

    Clean up obsolete api and netlify directories

--- api/toss-fail.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (req, res) => {
-    const { message, code } = req.query;
-    console.error(`Toss Payment Failed: ${code} - ${message}`);
-
-    return res.redirect(`/payment-cancel.html?error=${encodeURIComponent(message)}`);
-};
